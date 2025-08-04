36a575d1d1fa8e539c16f47a5e59f24c84a241d5
Samuel Rochcongar <samuel.rochcongar@teamwork.net>
2025-08-04 18:00:40 +0200

    FMT on s3-replication

--- registry/components/storage/s3-replication/variables.tf
+++ registry/components/storage/s3-replication/variables.tf
@@ -1,24 +1,24 @@
 variable "project_prefix" {
-  type = string 
+  type        = string
   description = "prefix used for all resources naming."
 }
 
 variable "source_bucket_name" {
-  type = string 
+  type        = string
   description = "Source bucket name."
 }
 
 variable "source_bucket_arn" {
-  type = string 
+  type        = string
   description = "Source bucket ARN."
 }
 
 variable "destination_bucket_name" {
-  type = string 
+  type        = string
   description = "Destination bucket name."
 }
 
 variable "destination_bucket_arn" {
-  type = string 
+  type        = string
   description = "Destination bucket ARN."
 }
