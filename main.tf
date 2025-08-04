e3619e0ca325c2c1ff0d10a77107f67fa7152a9d
Samuel Rochcongar <samuel.rochcongar@teamwork.net>
2025-08-04 18:15:33 +0200

    Rename module webapp1 into webapp-on-ecs-asg

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 # All kind of resources
-# module "interview_webapp1" {
-#   source = "./registry/app/webapp1"
+module "interview_webapp1" {
+  source = "./registry/app/webapp-on-ecs-asg"
 
-#   webapp_name     = "webapp-on-ecs"
-#   webapp_src_code = "${path.cwd}/src/webapp-on-ecs/"
-# }
-
-module "interview_webapp2" {
-  source = "./registry/app/webapp-on-s3-cdn"
-
-  profile          = var.profile
-  webapp_name      = "webapp-on-s3"
-  webapp_src_code  = "${path.cwd}/src/webapp-on-s3/react-app/"
-  secondary_region = var.secondary_region
-  deploy_dr        = true
-
-  providers = {
-    aws    = aws
-    aws.dr = aws.dr
-  }
+  webapp_name     = "webapp-on-ecs"
+  webapp_src_code = "${path.cwd}/src/webapp-on-ecs/"
 }
+
+# module "interview_webapp2" {
+#   source = "./registry/app/webapp-on-s3-cdn"
+
+#   profile          = var.profile
+#   webapp_name      = "webapp-on-s3"
+#   webapp_src_code  = "${path.cwd}/src/webapp-on-s3/react-app/"
+#   secondary_region = var.secondary_region
+#   deploy_dr        = true
+
+#   providers = {
+#     aws    = aws
+#     aws.dr = aws.dr
+#   }
+# }
