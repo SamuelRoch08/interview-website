aac9052114bc66f72d667c70b6cc375040ccc7ad
Samuel Rochcongar <samuel.rochcongar@teamwork.net>
2025-08-05 00:16:39 +0200

    Set ecs service name by variable

--- registry/components/compute/ecs-service/main.tf
+++ registry/components/compute/ecs-service/main.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_service" "service" {
-  name                 = "mongodb"
+  name                 = var.service_name
   cluster              = var.ecs_cluster_arn
   task_definition      = var.task_def_arn
   desired_count        = var.desired_count
