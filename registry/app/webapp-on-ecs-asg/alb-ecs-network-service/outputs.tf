7239f0b85fe9bb692af5e224f1e8e98fbab3b791
Samuel Rochcongar <samuel.rochcongar@teamwork.net>
2025-08-06 11:29:55 +0200

    Use private alb and cloudfront to private alb

--- registry/app/webapp-on-ecs-asg/alb-ecs-network-service/outputs.tf
+++ registry/app/webapp-on-ecs-asg/alb-ecs-network-service/outputs.tf
@@ -1,3 +1,7 @@
 output "lb_endpoint" {
   value = module.ecs_alb.lb_endpoint
 }
+
+output "lb_arn" {
+  value = module.ecs_alb.lb_arn
+}
--- registry/app/webapp-on-ecs-asg/main.tf
+++ registry/app/webapp-on-ecs-asg/main.tf
@@ -22,16 +22,30 @@ module "app_secondary_region" {
   cluster_min_size    = 1
   cluster_target_size = 1
   app_subnets         = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.11.0/24", "10.0.12.0/24"]
+  public_subnets       = ["10.0.11.0/24", "10.0.12.0/24"]
   data_subnets        = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
 module "cloudfront" {
-  source           = "../../components/network/cloudfront-alb"
+  source           = "../../components/network/cloudfront-alb-private"
   webapp_name      = var.webapp_name
   main_dns         = module.app_main_region.lb_endpoint
+  main_alb_arn     = module.app_main_region.lb_arn
+  main_alb_config = {
+    http_port = 80
+    https_port = 443
+    protocol_policy = "http-only"
+    ssl_protocols = ["TLSv1.2"]
+  }
   use_failover_dns = true
   failover_dns     = module.app_secondary_region.lb_endpoint
+  failover_alb_arn = module.app_secondary_region.lb_arn
+  failover_alb_config = {
+    http_port = 80
+    https_port = 443
+    protocol_policy = "http-only"
+    ssl_protocols = ["TLSv1.2"]
+  }
   allowed_methods  = ["GET", "HEAD"]
   default_ttl      = 10
 }
